29c968b83989d428b4ee06dc1160a58698e719f0
Dương Ngọc Hải <20521275@gm.uit.edu.vn>
2023-09-10 10:47:21 +0700

    done Using Basic Routing & MVC model

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const morgan = require('morgan')
 const path = require('path')
 const { engine } = require('express-handlebars');
+const exp = require('constants');
 const app = express();
+
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use(express.urlencoded({
+    extended:true
+}))
+app.use(express.json())
+
 
 //HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //Template engine
 app.engine('.hbs', engine({
@@ -17,8 +25,7 @@ app.engine('.hbs', engine({
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname,'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+//Route init
+route(app)
 
 app.listen(3000,()=> console.log(`App Listening...`));
