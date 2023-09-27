63c02e04bf4b3bfcbc54a2874c28da8eca734296
Dương Ngọc Hải <20521275@gm.uit.edu.vn>
2023-09-27 21:00:22 +0700

    update link mongodb

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/f8_education_dev', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        await mongoose.connect(
+            'mongodb+srv://12345:12345@cluster0.jyt8pn6.mongodb.net/?retryWrites=true&w=majority',
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        )
         console.log('Connect successfully!!!')
     } catch (error) {
         console.log('Connect failure!!!')
